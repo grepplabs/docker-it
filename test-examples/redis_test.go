83fbee159870ecd50ffd1da9bf41d2458701baa7
Michal Budzyn <michalbudzyn@gmail.com>
2017-07-13 22:25:31 +0200

    Add http wait

--- test-examples/redis_test.go
+++ test-examples/redis_test.go
@@ -2,6 +2,7 @@ package test_examples
 
 import (
 	"fmt"
+	"github.com/garyburd/redigo/redis"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
@@ -13,6 +14,14 @@ func TestRedisCall(t *testing.T) {
 	port, err := dockerEnvironment.Port("it-redis", "")
 	a.Nil(err)
 
-	fmt.Println("redis host", host)
-	fmt.Println("redis port", port)
+	conn, err := redis.Dial("tcp", fmt.Sprintf("%s:%s", host, port))
+	a.Nil(err)
+	defer conn.Close()
+
+	_, err = conn.Do("SET", "test-key", "test-value")
+	a.Nil(err)
+
+	value, err := redis.String(conn.Do("GET", "test-key"))
+	a.Nil(err)
+	a.EqualValues("test-value", value)
 }
